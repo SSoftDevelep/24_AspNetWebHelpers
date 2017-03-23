529003c41affc7ab16bb6e12e4d7f83228c56ee6
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-23 22:15:05 +0300

    Asp.Net MVC-Chart Devam

--- 24_AspNetWebHelpers/Controllers/MyChartController.cs
+++ 24_AspNetWebHelpers/Controllers/MyChartController.cs
@@ -16,9 +16,9 @@ namespace _24_AspNetWebHelpers.Controllers
             return View();
         }
 
-        public ActionResult ChartOlustur(string tip = "Column")
+        public ActionResult ChartOlustur(string tip = "Column", string cache = "chart1")
         {
-            Chart chart = Chart.GetFromCache("chart1");  //resmi cacheden okuduk.
+            Chart chart = Chart.GetFromCache(cache);  //resmi cacheden okuduk.
             if (chart == null)
             {
                 chart = new Chart(500, 500);
@@ -42,13 +42,45 @@ namespace _24_AspNetWebHelpers.Controllers
                 string imgPath = dir + "chart1.jpeg";
                 string xmlPath = dir + "chart1.xml";
 
-                chart.Save(imgPath, "png"); //resim dosyasi olarak kaydettik.
+                chart.Save(imgPath, format:"png"); //resim dosyasi olarak kaydettik.
                 chart.SaveXml(xmlPath);    //xml dosyasi olarak kaydettik.
-                chart.SaveToCache("chart1", 1, true);  //resmi cache'e attik. 1 dk sonra cache'de tutulur.
+                chart.SaveToCache(cache, 1, true);  //resmi cache'e attik. 1 dk sonra cache'de tutulur.
             }
 
 
             return View(chart);
+        }
+
+        public ActionResult ChartOlustur2(string tip = "Pie" ,string cache="chart2")
+        {
+            Chart chart = Chart.GetFromCache(cache);  //resmi cacheden okuduk.
+            if (chart == null)
+            {
+                chart = new Chart(500, 500);
+                chart.AddTitle("MyComputer-Urun Satis Detay Grafigi");
+                chart.AddLegend("Ürünler");
+                chart.AddSeries(name: "Ürünler", chartType: tip,
+                    xValue: new[] { "Bilgisayar", "Fare", "Klavye" },
+                    yValues: new[] { 100, 500, 300 });
+
+                string dir = Server.MapPath("~/charts/");
+
+                if (Directory.Exists(dir) == false)  //directory altinda charts klasoru yoksa olusturuyoruz.
+                {
+                    Directory.CreateDirectory(dir);
+                }
+
+                string imgPath = dir + "chart2.jpeg";
+                string xmlPath = dir + "chart2.xml";
+
+                chart.Save(imgPath, format:"png"); //resim dosyasi olarak kaydettik.
+                chart.SaveXml(xmlPath);    //xml dosyasi olarak kaydettik.
+                chart.SaveToCache(cache, 1, true);  //resmi cache'e attik. 1 dk sonra cache'de tutulur.
+            }
+
+
+            return View("ChartOlustur", chart);
+
         }
     }
 }
