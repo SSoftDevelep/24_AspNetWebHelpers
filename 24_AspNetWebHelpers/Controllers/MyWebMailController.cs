f53a4de192cb7799bfe1d66660f23850fe34c276
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-24 00:35:35 +0300

    AspNetMvc-WebMail ile E-Posta Islemleri

---
+++ 24_AspNetWebHelpers/Controllers/MyWebMailController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace _24_AspNetWebHelpers.Controllers
+{
+    public class MyWebMailController : Controller
+    {
+        // GET: MyWebMail
+        public ActionResult Index()
+        {
+            bool sonuc = false;
+
+            WebMail.SmtpServer = "smtp.gmail.com";
+            WebMail.SmtpPort = 587;
+            WebMail.UserName = "emre.solmaz106@gmail.com";
+            WebMail.Password = "bednam2018";
+
+            WebMail.EnableSsl = true;
+
+            string file = Server.MapPath("~/Images/sait.jpg");
+
+            try
+            {
+                WebMail.Send(
+                    to: "emresolmaz@outlook.com", subject: "Web Mail Test",
+                    body: "Bu bir web mail denemesidir.<br><b>Lütfen dikkate almayınız..</b>",
+                    replyTo: "dont-reply@outlook.com", isBodyHtml: true,
+                    filesToAttach: new[] { file });
+
+                sonuc = true;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Hata = ex.Message;
+            }
+
+            ViewBag.Sonuc = sonuc;
+
+            return View();
+            
+        }
+    }
+}
