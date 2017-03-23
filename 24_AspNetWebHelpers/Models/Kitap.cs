b9690ac1920a8a5fea216a428853002659c73279
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-24 02:34:52 +0300

    AspNetMvc-WebGrid ile Hazir Grid Sistemi

---
+++ 24_AspNetWebHelpers/Models/Kitap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _24_AspNetWebHelpers.Models
+{
+    public class Kitap
+    {
+        public static List<Kitap> Kitaplar { get; set; }
+        public Guid Id { get; set; }
+        public string Adi { get; set; }
+        public string Yazar { get; set; }
+        public DateTime YayinTarihi { get; set; }
+        public decimal Fiyat { get; set; }
+
+        protected Kitap()
+        {
+
+        }
+
+        public static void GenerateFakeData(int count)
+        {
+            if (Kitaplar==null)
+            {
+                Kitaplar = GetFakeData(count);
+            }
+        }
+
+        private static List<Kitap> GetFakeData(int count)
+        {
+            List<Kitap> kitaplar = new List<Models.Kitap>();
+
+            for (int i = 0; i < count; i++)
+            {
+                kitaplar.Add(new Kitap()
+                {
+                    Id = Guid.NewGuid(),
+                    Adi = FakeData.NameData.GetCompanyName(),
+                    Yazar = FakeData.NameData.GetFullName(),
+                    YayinTarihi = FakeData.DateTimeData.GetDatetime(DateTime.Now.AddYears(-2), DateTime.Now.AddMonths(-1)),
+                    Fiyat = FakeData.NumberData.GetNumber(8, 99)
+                });
+            }
+            return kitaplar;
+        }
+    }
+}
