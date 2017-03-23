c5f4d6b8ac46454334c1453602f8f0e713437ad3
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-23 23:48:34 +0300

    AspNetMvc-WebCache ile Performans

---
+++ 24_AspNetWebHelpers/Controllers/MyWebCacheController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace _24_AspNetWebHelpers.Controllers
+{
+    public class MyWebCacheController : Controller
+    {
+        // GET: MyWebCache
+        public ActionResult Index()
+        {
+            string time = WebCache.Get("zaman");  //zaman anahtarinin icindeki degeri getir.
+
+            if (string.IsNullOrEmpty(time))
+            {
+                time = DateTime.Now.ToString();
+                WebCache.Set(key: "zaman", value: time, minutesToCache: 1, slidingExpiration: true);
+            }
+            ViewBag.Simdi = time;
+
+            return View();
+        }
+
+        public ActionResult RemoveCache()
+        {
+            WebCache.Remove("zaman");
+            return RedirectToAction("Index");
+        }
+    }
+}
