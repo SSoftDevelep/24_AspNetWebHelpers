4a6b82e98968d81a903a751f3128583486c7592f
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-23 15:40:27 +0300

    AntiForgery ile Guvenlik

---
+++ 24_AspNetWebHelpers/Controllers/AntiForgeryController.cs
@@ -0,0 +1,31 @@
+﻿using _24_AspNetWebHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _24_AspNetWebHelpers.Controllers
+{
+    public class AntiForgeryController : Controller
+    {
+        
+        public ActionResult Test()
+        {
+            return View(Veritabani.veriler);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]  //olusturalan kodun kontrolunu saglariz.eslesme dogru olursa post olur.
+        public ActionResult Test(int id)
+        {
+            Veritabani.veriler.RemoveAt(id);
+            return RedirectToAction("Test");
+        }
+
+        public ActionResult FakeTest()
+        {
+            return View();
+        }
+    }
+}
---
+++ 24_AspNetWebHelpers/Models/Veritabani.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _24_AspNetWebHelpers.Models
+{
+    public static class Veritabani
+    {
+        public static List<string> veriler =
+            new List<string>(new string[] { "Mazhar", "Fuat", "Ozkan" });
+
+    }
+
+}
